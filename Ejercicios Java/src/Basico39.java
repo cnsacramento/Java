fdc8fb9aa190f42352219a04446fd6f653eca3eb
Christian Novo Sacramento <christianivh366@gmail.com>
2022-02-03 16:28:36 +0000

    Agregado el ejercicio 39

---
+++ Ejercicios Java/src/Basico39.java
@@ -0,0 +1,16 @@
+/*
+Programa Java que muestre los números del 100 al 1 utilizando la instrucción while.
+*/
+/**
+ * @author Christian Novo Sacramento
+ */
+public class Basico39 {
+    public static void main(String[] args) {
+        
+        int i = 100;
+        while(i >= 1) {
+            System.out.println(i);
+            i--; //Se le va restando 1 a i
+        }
+    }
+}
