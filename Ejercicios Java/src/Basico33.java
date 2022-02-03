1f56ae70f10c346ded18d5e011363b2bef8d359f
Christian Novo Sacramento <christianivh366@gmail.com>
2022-02-03 16:12:38 +0000

    Modificado el ejercicio 33

--- Ejercicios Java/src/Basico33.java
+++ Ejercicios Java/src/Basico33.java
@@ -22,6 +22,34 @@ public class Basico33 {
         if (mes < 0 || mes > 12) {
             System.out.println("ERROR!!! (El número del mes es incorrecto)");
         }else{
+            //Como hay que mostrar el nombre del mes se puede hacer uso de un switch
+            switch(mes) {
+                case 1: System.out.print("El mes es: enero y "); //Se le quita el ln para que se junte con la cantidad de días
+                    break; //Los break sirven para que no de lugar la siguiente instrucción
+                case 2: System.out.print("El mes es: febrero y "); 
+                    break;
+                case 3: System.out.print("El mes es: marzo y ");
+                    break;
+                case 4:System.out.print("El mes es: abril y ");
+                 break;
+                case 5: System.out.print("El mes es: mayo y ");
+                    break;
+                case 6: System.out.print("El mes es: junio y ");
+                    break;
+                case 7: System.out.print("El mes es: julio y ");
+                    break;
+                case 8: System.out.print("El mes es: agosto y ");
+                    break;
+                case 9: System.out.print("El mes es: septiembre y ");
+                    break;
+                case 10:System.out.print("El mes es: octubre y ");
+                    break;
+                case 11: System.out.print("El mes es: noviembre y ");
+                    break;
+                case 12: System.out.print("El mes es: diciembre y ");
+                    break;
+                //Normalmente aquí va un default pero como se quito la opción que induce a error no hace falta
+            }
             if (mes == 4 || mes == 6 || mes == 9 || mes == 11) { //Los meses con 30 días son abril, junio, septiembre y noviembre
                 System.out.println(" es un mes de 30 días"); 
             }else if(mes == 2) { //El único mes que tiene 28 es febrero, exceptuando los años bisiestos
