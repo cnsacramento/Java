a68519efa25964b5998b23fcf01f88b5d12ad149
Christian Novo Sacramento <christianivh366@gmail.com>
2022-02-03 16:26:19 +0000

    Agregado el ejercicio 36

---
+++ Ejercicios Java/src/Basico36.java
@@ -0,0 +1,14 @@
+/*
+Programa Java que muestre los números del 1 al 100 utilizando la instrucción for.
+ */
+/**
+ * @author Christian Novo Sacramento
+*/
+public class Basico36 {
+    public static void main(String[] args) {
+        
+        for (int i = 0; i <= 100; i++) { //En los bucles for se declara todo en la condición
+            System.out.println(i);
+        }
+    }
+}
