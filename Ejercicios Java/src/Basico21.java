e9940b65e91fc29db08087caf8755ca3bdc6fe85
Christian Novo Sacramento <christianivh366@gmail.com>
2022-02-03 09:31:21 +0000

    Agregado ejercicio 21

---
+++ Ejercicios Java/src/Basico21.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+/**
+ * @author Christian Novo Sacramento
+ */
+/*
+Programa que calcule el precio de venta de un producto conociendo el precio por unidad (sin IVA) del producto,
+ el número de productos vendidos y el porcentaje de IVA aplicado. Los datos anteriores se leerán por teclado. 
+*/
+public class Basico21 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int precioUnidad, productosVendidos;
+        double iva;
+
+        //Se solicitan los datos
+        System.out.println("¿Cuánto es el precio por unidad?");
+        precioUnidad = sc.nextInt();
+        System.out.println("¿Cuál es el número de productos vendidos?");
+        productosVendidos = sc.nextInt();
+        System.out.println("¿Cuál es el porcentaje del IVA?");
+        iva = sc.nextDouble();
+
+        //Se calcula el precio de venta
+        double ivaPrecioVenta = (precioUnidad * productosVendidos) * (iva/100);
+        double precioVentaConIva = (precioUnidad * productosVendidos) + ivaPrecioVenta;
+
+        //Se muestra el resultado
+        System.out.println();
+        System.out.println("FACTURA");
+        System.out.println("=======");
+        System.out.println("El total del iva aplicado es: " + ivaPrecioVenta);
+        System.out.println("El precio total de venta con iva es: " + precioVentaConIva);
+    }
+}
