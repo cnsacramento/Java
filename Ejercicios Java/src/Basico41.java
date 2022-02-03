cd8aef806983cd161288a7f6651b8e029a284331
Christian Novo Sacramento <christianivh366@gmail.com>
2022-02-03 16:33:26 +0000

    Agregado ejercicio 41

---
+++ Ejercicios Java/src/Basico41.java
@@ -0,0 +1,14 @@
+/*
+Programa Java que muestre los números del 100 al 1 utilizando la instrucción for.
+*/
+/**
+ * @author Christian Novo Sacramento
+ */
+public class Basico41 {
+    public static void main(String[] args) {
+        
+        for (int i = 100; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
