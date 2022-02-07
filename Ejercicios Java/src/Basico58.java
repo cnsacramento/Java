490dc3c706b71d0ee3ffe34d9c81d4f6c0f7ddb8
cnsacramento <christianivh366@gmail.com>
2022-02-07 19:50:14 +0000

    Se avanza en la practica 58 (numeros romanos)

--- Ejercicios Java/src/Basico58.java
+++ Ejercicios Java/src/Basico58.java
@@ -11,13 +11,21 @@ El programa pide un número entre 1 y 3999 y calcula su equivalente en números 
  * @author Christian Novo Sacramento
  */
 public class Basico58 {
+    /**
+     * Método que convierte los arabigos a romanos
+     * @param numero arabigo a convertir en romano
+     * @return un String con el número romano
+     */
+    public static String convertirANumerosRomanos(int numero) {
+
+        return "Prueba";
+    }
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
         int numeroArabigo; //Número que utilizamos a diario
         String numeroRomano;
 
-        
         System.out.println("|||||||||||||||||||||||||||||");
         System.out.println("|| CONVERSOR NÚMERO ROMANO ||");
         System.out.println("|||||||||||||||||||||||||||||");
@@ -28,7 +36,13 @@ public class Basico58 {
             System.out.printf("Número: ");
             numeroArabigo = sc.nextInt();
         }
+        int cifras = 1; //Almacena la cantidad de cifras
+        while(numeroArabigo / 10 > 0) { //Halla la cantidad de dígitos del número
+            numeroArabigo = numeroArabigo / 10;
+            cifras++;
+        }
 
-        
+        numeroRomano = convertirANumerosRomanos(numeroArabigo); //Variable que guarda el resultado del método conversor
+        System.out.println(numeroRomano);
     }
 }
